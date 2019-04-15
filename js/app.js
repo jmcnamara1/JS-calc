e4cf44838f763eed71cd9174f362fdb7a6dea4db
Tech-a50 <tech-a50@tech-a50s-air.home>
2019-04-15 17:59:20 +0100

    feature 1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// prompt("This ")
+var choice1 = prompt("Please type the first number you would like to use in the calculation")
+var choice2 = prompt("Please type the second number you would like to use below")
+
+var answerSum = Number(choice1) + Number(choice2);
+
+prompt("The answer is " + answerSum);
